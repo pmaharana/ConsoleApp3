e76864d1f65d9b6945482b5b0cd8673fbf47d5af
Pranay Maharana <pranay.maharana@gmail.com>
2017-06-05 20:10:59 -0400

    --first commit

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+
+
+    {
+
+        //Hacker rank pangram
+
+        public static string Pangram(string sentence)
+        {
+            var comparison = new List<char> { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+                'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+            };
+
+            var input = sentence.ToLower();
+            int count = 0;
+
+            foreach (var item in comparison)
+            {
+                if (input.Contains(item.ToString()))
+                {
+                    count++;
+                }
+            }
+
+            if (count == 26)
+            {
+                return "pangram";
+            }
+            else
+            {
+                return "not pangram";
+            }
+        }
+            
+
+
+
+
+
+        public static string FizzBuzz(string sentence)
+        {
+            var rv = String.Empty;
+            var upperVowel = new List<char> { 'A', 'E', 'I', 'O', 'U' };
+            var lowerVowel = new List<char> { 'a', 'e', 'i', 'o', 'u' };
+            
+
+            foreach (var letter in sentence)
+            {
+                if (upperVowel.Contains(letter))
+                {
+                    rv += "Iron Yard";
+                }
+                else if (char.IsUpper(letter) && !upperVowel.Contains(letter))
+                {
+                    
+                    rv += "Iron";
+                }
+                else if (lowerVowel.Contains(letter))
+                {
+                    
+                    rv += "Yard";
+                }
+                else
+                {
+                    rv += letter;
+
+                }
+            }
+
+            return rv;
+        }
+                    
+
+
+
+
+
+
+
+
+        public static bool XO(string input)
+        {
+            var lower = input.ToLower();
+            int xCount = lower.Count(c => c == 'x');
+            int oCount = lower.Count(c => c == 'o');
+
+
+
+            if (lower.Contains("x") && lower.Contains("o") && xCount == oCount)
+            {
+                return true;
+            }
+            if (!lower.Contains("x") && !lower.Contains("o"))
+            {
+                return true;
+            }
+            else
+            {
+                return false;    
+            }
+        }
+
+
+        public static string repeatStr(int n, string s)
+        {
+            string rv = "";
+            for (int i = 0; i < n; i++)
+            {
+                rv += s;
+            }
+            return rv;
+        }
+
+        public static List<string> wave(string str)
+        {
+            List<string> yugi = new List<string>();
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (char.IsLetter(str[i]))
+                {
+
+                    yugi.Add(str.Substring(0, i) + str.Substring(i, 1).ToUpper() + str.Substring(i + 1));
+                }
+            }
+            return yugi;
+        }
+                
+
+
+
+
+        static void Main(string[] args)
+        {
+
+            //var input = Console.ReadLine().Split(' ').Select(s => long.Parse(s));
+            //input.Reverse();
+            //var min = input.Sum() - input.Max();
+            //var max = input.Sum() - input.Min();
+            //Console.WriteLine($"{min} {max}");
+
+            //string s = Console.ReadLine();
+            //int count = 1;
+            //for (int i = 0; i < s.Length; i++)
+            //{
+            //    if (char.IsUpper(s[i]))
+            //    {
+            //        count++;
+            //    }
+            //}
+            //Console.WriteLine(count);
+
+            //string s = Console.ReadLine();
+            //int count = 1;
+            //var answer = s.Count(i => char.IsUpper(i));
+
+            //var neya = int.Parse(Console.ReadLine());
+
+            //var answer = (neya < 0) ? "negative" : "positibe";
+            //Console.WriteLine(answer);
+
+            //char ar = 'a';
+            //var art = ar++;
+            //Console.WriteLine(art++);
+            //Console.WriteLine(art++);
+            //Console.WriteLine(art++);
+            //Console.WriteLine(art++);
+
+
+            //int n = Convert.ToInt32(Console.ReadLine());
+            //string s = Console.ReadLine();
+            //int k = Convert.ToInt32(Console.ReadLine());
+
+            ////var answer = s.Select(o => o++).Where(w ;
+            //var newAnswer = "";
+            ////Console.WriteLine(newAnswer);
+
+            //for (int i = 0; i < s.Length; i++)
+            //{
+            //    newAnswer += i++;
+
+            //}
+
+            //Console.WriteLine(newAnswer);
+
+            //var result = XO("xoxoxo");
+            //Console.WriteLine(result);
+
+
+            ////Code Kata TIY Fizz Buzz
+            //var result = FizzBuzz("A");
+            //Console.WriteLine(result);
+
+
+
+            ////Hacker Rank Pangram
+            //Console.WriteLine(Pangram("We promptly judged antique ivory buckles for the next prize"));
+
+            //string hel = "two words";
+            //Console.WriteLine(hel.Substring(0, 4) + hel.Substring(4, 1).ToUpper() + hel.Substring(4 + 1));
+
+            var rv = wave("two cord");
+            rv.ForEach(f => Console.WriteLine(f));
+
+
+        }
+    }
+}
